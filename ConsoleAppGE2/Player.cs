099339d19156d69c9ad0cf35f2ed71ac12058a17
S-JOBI23-058\student <231064@st.yoshida-g.ac.jp>
2024-06-14 11:04:59 +0900

    PlayerクラスにPlayer情報作成用のメソッドを作成 #3 PlayerクラスにPlayer情報表示用のメソッドを作成 #4 Main関数で、２人分のプレイヤー情報登録ができるようにする #5 Main関数で、5.で登録したプレイヤーの情報を表示できるようにする #6

--- ConsoleAppGE2/Player.cs
+++ ConsoleAppGE2/Player.cs
@@ -6,6 +6,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.NetworkInformation;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -13,6 +14,30 @@ namespace ConsoleAppGE2
 {
     internal class Player
     {
+        private string name = "";               //名前
+        private int hp;                         //HP
+        private int atk;                        //攻撃力
+        private const int MAX_PLAYER = 2;       //登録する最大人数
 
+
+        //プレイヤー情報作成関数
+        public void CreatePlayer()
+        {
+            Console.Write("名前を入力してください＞");
+            name = Console.ReadLine();
+            //hpとatkはstringからint型に変換
+            Console.Write("HPを入力してください＞");
+            int.TryParse(Console.ReadLine(), out hp);
+            Console.Write("攻撃力を入力してください＞");
+            int.TryParse(Console.ReadLine(), out atk);
+            Console.Clear();
+        }
+
+        //プレイヤー情報表示関数
+        public void ShowPlayer()
+        {
+            //表示する
+            Console.WriteLine("名前：{0}、HP：{1}、攻撃力：{2}", name, hp, atk);
+        }
     }
 }
--- ConsoleAppGE2/Program.cs
+++ ConsoleAppGE2/Program.cs
@@ -13,9 +13,37 @@ namespace ConsoleAppGE2
 {
     internal class Program
     {
+        private const int MAX_PLAYER = 2;           //プレイヤーの最大数
+
         static void Main(string[] args)
         {
-            
+            Player[] players = new Player[MAX_PLAYER];
+
+            //人数分プレイヤークラスの配列をインスタンス
+            for (int i = 0; i < MAX_PLAYER; i++)
+            {
+                players[i] = new Player();
+            }
+
+            //人数分登録関数を呼ぶ
+            for (int i = 0;i < MAX_PLAYER;i++)
+            {
+                Console.WriteLine("{0}人目の登録", i + 1);
+                players[i].CreatePlayer();
+            }
+
+            //人数分表示関数を呼ぶ
+            for(int i = 0;i < MAX_PLAYER; i++)
+            {
+                Console.WriteLine("{0}人目", i + 1);
+                players[i].ShowPlayer();
+                Console.WriteLine();
+            }
+
+
+            //キー入力待ち
+            Console.WriteLine("ENTERを押して終了");
+            Console.ReadLine();
         }
     }
 }
