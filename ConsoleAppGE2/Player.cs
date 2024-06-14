73ae429f97b415ff070be00a778abf33f3a32d9d
S-JOBI23-058\student <231064@st.yoshida-g.ac.jp>
2024-06-14 09:32:12 +0900

    VisualStudio2022でプロジェクトを作成 #1

---
+++ ConsoleAppGE2/Player.cs
@@ -0,0 +1,21 @@
+﻿//=========================================
+//開発技法
+//2024/6/14
+//三宅歩人
+//=========================================
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppGE2
+{
+    internal class Player
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
